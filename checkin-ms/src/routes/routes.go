7c7ab270516f3260ba644954df0bf5bd2c794aed
Pablo ruan dos santos <pablords@gmail.com>
2022-08-08 23:04:18 -0300

    feat: updated route health

--- checkin-ms/src/routes/routes.go
+++ checkin-ms/src/routes/routes.go
@@ -12,7 +12,7 @@ func Routes() *mux.Router {
 
 	api := r.PathPrefix("/api/v1").Subrouter()
 
-	api.HandleFunc("", controllers.HeathCheckController).Methods(http.MethodGet)
+	api.HandleFunc("/health", controllers.HeathCheckController).Methods(http.MethodGet)
 	api.HandleFunc("/access-release/{id}", controllers.AccesReleaseController).Methods(http.MethodGet)
 
 	return api
